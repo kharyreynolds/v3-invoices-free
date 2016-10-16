dca6e7731d8811fa05338e1a075704f94c656b9b
Khary Reynolds <ceo@virtualcubed.com>
2016-10-16 10:36:48 -0500

    Adding File Structure

---
+++ v3-invoices-free.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Plugin Name: v3 Invoices Free Plugin
+Plugin URI: https://www.virtualcubed.com/vc-invoice
+Description: Send online invoices & accept payments with Stripe
+Version: 1.0.0
+Author: Virtual Cubed Technologies
+Author URI: https://www.virtualcubed.com
+*/
+
+
+
+ 
